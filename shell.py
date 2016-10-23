9fe246a3480f36ed21d63b06666f1a70498e6589
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-10-23 16:20:28 +0530

    Added preloop

--- shell.py
+++ shell.py
@@ -4,6 +4,11 @@ import os
 
 class Shell(Cmd):
 	
+	def preloop(self):
+		print 'A Simple python Shell created by Avais and Swapneel.\nUse help or "?" to see list of commands.\nUse help (command name) to for help realted to the command'
+		print 'Version 0.1'
+		print '==============================================\n\n\t\tStay in the shell\n\n=============================================='
+
 	def do_lf(self, arg):
 		'Lists Files in the present directory'
 		for f in os.listdir('.'):
