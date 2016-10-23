4221bfe4a9d2c6cd327a14fe9aa9dec6793b8f4d
Avais Pagarkar <avaispagarkar123@gmail.com>
2016-10-23 17:06:23 +0530

    Added create directory

--- shell.py
+++ shell.py
@@ -1,6 +1,15 @@
 from cmd2 import Cmd 
 import os
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 class Shell(Cmd):
 	
@@ -12,12 +21,20 @@ class Shell(Cmd):
 	def do_lf(self, arg):
 		'Lists Files in the present directory'
 		for f in os.listdir('.'):
-			print f
+			if os.path.isfile(f):
+				print f
+			else:
+				print bcolors.OKBLUE + f + bcolors.ENDC
 
 	def do_exit(self, arg):
 		'Exits the shell'
 		print 'Thanks for using this amazingly simple shell.'
 		return True
+
+	def do_crdr(self, arg):
+		'Creates a Directory'
+		os.mkdir(arg)
+		print 'Directory named {0} created'.format(arg)
 
 
 shell = Shell()
